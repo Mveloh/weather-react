d224021f21f2ba753f3add1de830b1ed91a7c7d9
mveloh <mvelozuluu@gmail.com>
2023-09-06 21:33:49 +0200

    Updated weather app

--- src/Weather.js
+++ src/Weather.js
@@ -32,59 +32,61 @@ export default function Weather(props) {
   //);
 
   return (
-    <div className="Weather">
-      <form className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city.."
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="overview">
-        <h1>{temperatureData.city}</h1>
-        <ul>
-          <li>Last updated: {temperatureData.date}</li>
-          <li>{temperatureData.description}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix weather-temperature">
-            <img
-              src={temperatureData.imgUrl}
-              alt={temperatureData.description}
-              className="float-left"
-            />
-            <div className="float-left">
-              <strong>{temperatureData.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
+    <div className="Weather-app">
+      <div className="Weather">
+        <form className="mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                className="form-control"
+                autoComplete="off"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
-        <div className="col-6">
+        </form>
+        <div className="overview">
+          <h1>{temperatureData.city}</h1>
           <ul>
-            <li>Humidity: {temperatureData.humidity}%</li>
-            <li>Wind: {temperatureData.wind} km/h</li>
+            <li>Last updated: {temperatureData.date}</li>
+            <li>{temperatureData.description}</li>
           </ul>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={temperatureData.imgUrl}
+                alt={temperatureData.description}
+                className="float-left"
+              />
+              <div className="float-left">
+                <strong>{temperatureData.temperature}</strong>
+                <span className="units">
+                  <a href="/">°C</a> | <a href="/">°F</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {temperatureData.humidity}%</li>
+              <li>Wind: {temperatureData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
       <small>
         <a
-          href="https://github.com/Mveloh/final-weather-forecast"
+          href="https://github.com/Mveloh/weather-react"
           target="_blank"
           rel="noreferrer"
         >
